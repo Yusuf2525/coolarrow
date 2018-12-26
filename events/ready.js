a7fb092cf2cdaaaedf349eba36043b9d4ffea0e4
Yusuf2525 <46084253+Yusuf2525@users.noreply.github.com>
2018-12-27 00:06:08 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,20 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setGame(`..yardım|Türkiye`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-};
+   var oyun = [
+        "Bot Saldırıya Uğramıştır Sunuculara Gelen Kötü Olaylar Yüzünden Özür Dileriz ",
+        "Destek sunucumuz saldırıya uğradı sunucumuza gelmek için ca!yardımsunucum yazabilirsiniz",
+        "💪 7/24 Aktif!",  
+        "💡 ca!davet | Botumuzu ekleyin",
+        "👨 Size Hizmet Vermekten Mutluluk duyuyoruz",
+        "🌍 Sunucu Ayarları İçin ca!sunucu",
+        "ca!yardım 🔥 + ca!davet 🔥 + ca!öneri"
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setGame(oyun[random], "https://www.twitch.tv/emirhansaracyt");
+        }, 2 * 2500);
+}
