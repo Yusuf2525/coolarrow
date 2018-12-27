0e866ab0f63bb0b7c740007309a3379d6e8a6e98
Yusuf2525 <46084253+Yusuf2525@users.noreply.github.com>
2018-12-27 20:07:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,4 +197,15 @@ client.on('message', msg => {
 }
 });
 
+    client.on('message', async msg => {
+    if (msg.content.toLowerCase() === prefix + "rainbow") {
+   if (msg.channel.type === "dm") return;
+  const rol = 'Rainbow' // Rol ismi buraya
+  setInterval(() => {
+      msg.guild.roles.find(s => s.name === rol).setColor("RANDOM")
+      }, 350);
+  }
+});
+ 
+
 client.login(process.env.BOT_TOKEN);
