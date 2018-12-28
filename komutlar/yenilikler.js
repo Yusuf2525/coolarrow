6bfe3effddb917214aaa5f9b0dc72a244fa05bbb
Yusuf2525 <46084253+Yusuf2525@users.noreply.github.com>
2018-12-28 22:40:24 +0300

    Update yenilikler.js

--- komutlar/yenilikler.js
+++ komutlar/yenilikler.js
@@ -7,7 +7,7 @@ exports.run = (client, message, params) => {
   const embed = new Discord.RichEmbed()
   .setDescription('')
   .setColor(0x00ffff)
-  .addField("**» Yenilikler**", `ca!yardım Komutları Yenilendi Bakmak için *ca!yardım* Yazınız. \nca!nah Komutu Eklendi Bakmak İçin *ca!yardım* Yazınız. \nca!stresçarkı Komutu Eklendi Bakmak İçin *ca!yardım* Yazınız. \nca!öneri Komutu Eklendi Bakmak İçin *ca!yardım* Yazınız. \nca!emojiyazı Komutu Eklendi Bakmak İçin *ca!yardım* Yazınız. \nca!report , ca!ban , ca!sustur Komutları Yenilendi Bakmak İçin *ca!yardım* Yazınız. `)
+  .addField("**» Yenilikler**", `ca!yardım Komutları Yenilendi Bakmak için *ca!yardım* Yazınız. \nca!nah Komutu Eklendi Bakmak İçin *ca!yardım* Yazınız. \nca!tkm Komutu Eklendi Bakmak İçin *ca!yardım* Yazınız. \nca!havadurumu Komutu Eklendi Bakmak İçin *ca!yardım* Yazınız. \nca!stresçarkı Komutu Eklendi Bakmak İçin *ca!yardım* Yazınız. \nca!öneri Komutu Eklendi Bakmak İçin *ca!yardım* Yazınız. \nca!emojiyazı Komutu Eklendi Bakmak İçin *ca!yardım* Yazınız. \nca!report , ca!ban , ca!sustur Komutları Yenilendi Bakmak İçin *ca!yardım* Yazınız. `)
 
 
   return message.channel.sendEmbed(embed);
